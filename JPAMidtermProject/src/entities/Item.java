bc558eb26ff48a01bdd4394f7958ea5943420ec6
Alexis Low <alexisthelow@gmail.com>
2017-11-29 16:23:01 -0700

    added item entity

---
+++ JPAMidtermProject/src/entities/Item.java
@@ -0,0 +1,103 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "item")
+public class Item {
+	
+	@Id
+	@GeneratedValue()
+	private int id;
+	
+	private String title;
+	
+	private String description;
+	
+	@JoinColumn(name = "owner_id")
+	private int ownerId;
+	
+	@JoinColumn(name = "is_supply")
+	private boolean isSupply;
+	
+	private boolean available;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public boolean isSupply() {
+		return isSupply;
+	}
+
+	public void setSupply(boolean isSupply) {
+		this.isSupply = isSupply;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", title=" + title + ", description=" + description + ", ownerId=" + ownerId
+				+ ", isSupply=" + isSupply + ", available=" + available + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	
+	
+}
