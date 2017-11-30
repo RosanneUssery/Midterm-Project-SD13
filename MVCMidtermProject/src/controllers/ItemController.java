81f0ce946db67ec3658ef0904e64c6af20141be7
Alexis Low <alexisthelow@gmail.com>
2017-11-30 16:09:14 -0700

    framed out controllers

--- MVCMidtermProject/src/controllers/ItemController.java
+++ MVCMidtermProject/src/controllers/ItemController.java
@@ -15,12 +15,12 @@ public class ItemController {
 	
 	//return a view with filtered list of items -- GET
 	
-	//return a view with item detail
+	//return a view with single item detail -- GET
 	
 	//send user to page where they can add an item -- POST REDIRECT
 		//send user to page with added item
 	
-	//return a page with a user and all their items
+	//return a page with a user and all their items -- GET
 	
 	//send user to page where they can update item --POST REDIRECT
 		//send user to page with updated item
