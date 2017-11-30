d61ffd7ca99720579589c1d0a0146a3c3bb8cef9
Kittygomeowr <rkussery@gmail.com>
2017-11-30 08:34:06 -0700

    wireframe folder added

--- JPAMidtermProject/test/test/AddressTest.java
+++ JPAMidtermProject/test/test/AddressTest.java
@@ -1,23 +1,37 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
-import com.sun.jndi.cosnaming.IiopUrl.Address;
+import entities.Address;
+
 
 public class AddressTest {
 
-	@PersistenceContext
+//	@PersistenceContext
+	private EntityManagerFactory emf;
 	private EntityManager em;
 	private Address add;
 
 	@Before
 	public void setUp() throws Exception {
+		this.emf = Persistence.createEntityManagerFactory("MidtermPU");
+		this.em = emf.createEntityManager();
+		
 		add = em.find(Address.class, 1);
 
+	}
+	
+	@Test
+	public void address_has_id() {
+		assertEquals(1, add.getId());
 	}
 
 	@After
