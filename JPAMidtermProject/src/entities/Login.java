1e914ab1e400e7093bc2fa8ae1fed17ee331499e
Jason Caldwell <jdcald13@gmail.com>
2017-11-30 11:49:03 -0700

    Create Login entity.

---
+++ JPAMidtermProject/src/entities/Login.java
@@ -0,0 +1,64 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "login")
+public class Login {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_email")
+	private String userEmail;
+	
+	private String pwd;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	@Override
+	public String toString() {
+		return "Login [userEmail=" + userEmail + ", pwd=" + pwd + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((userEmail == null) ? 0 : userEmail.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		if (userEmail == null) {
+			if (other.userEmail != null)
+				return false;
+		} else if (!userEmail.equals(other.userEmail))
+			return false;
+		return true;
+	}
+	
+	
+
+}
