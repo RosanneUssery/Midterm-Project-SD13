c788878533e703116d12ce6252920ce11b8b9ba1
Alexis Low <alexisthelow@gmail.com>
2017-11-29 17:06:33 -0700

    added activity entity

---
+++ JPAMidtermProject/src/entities/Activity.java
@@ -0,0 +1,100 @@
+package entities;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activity")
+public class Activity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@OneToOne
+	@JoinColumn(name = "borrower_id")
+	private User borrower;
+	
+	@OneToOne
+	@JoinColumn(name = "item_id")
+	private Item item;
+	
+	@JoinColumn(name = "date_lent")
+	private Date dateLent;
+	
+	@JoinColumn(name = "due_date")
+	private Date dueDate;
+
+	public User getBorrower() {
+		return borrower;
+	}
+
+	public void setBorrower(User borrower) {
+		this.borrower = borrower;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
+	public Date getDateLent() {
+		return dateLent;
+	}
+
+	public void setDateLent(Date dateLent) {
+		this.dateLent = dateLent;
+	}
+
+	public Date getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", borrower=" + borrower + ", item=" + item + ", dateLent=" + dateLent
+				+ ", dueDate=" + dueDate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+
+}
