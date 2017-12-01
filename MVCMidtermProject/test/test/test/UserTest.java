37d1b16df01e6216a9d2942b547e7bd3a4f84eaf
Gregory Norwood, Jr <gmajorincome@gmail.com>
2017-12-01 09:44:38 -0700

    User/Login passing tests in MVC

--- MVCMidtermProject/test/test/test/UserTest.java
+++ MVCMidtermProject/test/test/test/UserTest.java
@@ -1,24 +1,29 @@
 package test.test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
+import entities.Login;
 import entities.User;
 
 public class UserTest {
 	private EntityManagerFactory emf;
 	private EntityManager em;
 	private User user;
+	private Login login;
 
 	@Before
 	public void setUp() throws Exception {
 		this.emf = Persistence.createEntityManagerFactory("MidtermPU");
 		this.em = emf.createEntityManager();
-		user = em.find(User.class, 1);
+		login = em.find(Login.class, "first@first.com");
 
 	}
 	
@@ -28,6 +33,14 @@ public class UserTest {
 	public void tearDown() throws Exception {
 		this.em.close();
 		this.emf.close();
-		user = null;
+		login = null;
 	}
+	
+	
+	@Test
+	public void test_user_login_email() {
+    	assertEquals("first@first.com",login.getUserEmail());
+    	assertEquals("1",login.getPwd());
+	}
+	
 }
