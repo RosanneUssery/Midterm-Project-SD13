2a4287d4a684e3ff9128818c12655b968584ad4a
Alexis Low <alexisthelow@gmail.com>
2017-12-05 17:09:06 -0700

    userDAOimpl cleanup

--- MVCMidtermProject/src/data/UserDAOImpl.java
+++ MVCMidtermProject/src/data/UserDAOImpl.java
@@ -52,30 +52,6 @@ public class UserDAOImpl implements UserDAO {
 		return newUser;
 	}
 
-//	@Override
-//	public User createUser(User user, Address address, Login login) {
-//		
-//		//first insert the address, get back the id
-//        em.persist(address);
-//        em.flush();
-//        
-//        //attach address and email to user
-//        user.setAddress(address);
-//        user.setEmail(login.getUserEmail());
-//        System.out.println("userDAOImpl.createUser says this is the login object's email:" + login.getUserEmail());
-//        System.out.println("userDAOImpl.createUser says this is the user object's email:" + user.getEmail());
-//        //persist user before login, because login table depends on user email being present
-//        em.persist(user);
-//        
-//        //now we can persist login
-//        em.persist(login);
-//
-//        //get it all back
-//        em.flush();
-//        
-//		return user;
-//	}
-
 	@Override
 	public List<User> getAllUsers() {
 		
